857771dd55a9459b87b2665fa764e05c93245127
Alison Wu <alison.wu23@gmail.com>
2025-05-12 14:02:52 +1000

    refactor: add user routes for profile apis

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,15 +2,11 @@ const express = require("express");
 const {
   register,
   login,
-  getProfile,
-  updateProfile,
 } = require("../controllers/authController");
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/profile", protect, getProfile);
-router.post("/profile", protect, updateProfile);
 
 module.exports = router;
