87c4ab3f8bcf95c31a0723b50fe4e3cbfae627c8
Alison Wu <alison.wu23@gmail.com>
2025-05-14 00:31:32 +1000

    feat: added employee, and then generate registration link for user to register themselves.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,17 +2,22 @@ import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axiosInstance from "../api/axiosConfig";
 import { jwtDecode } from "jwt-decode";
+import {
+  NotificationContainer,
+  useNotification,
+} from "../components/NotificationBar";
 
 const Register = () => {
   const [formData, setFormData] = useState({
+    email: "",
     token: "",
     password: "",
   });
   const [registerInfo, setRegisterInfo] = useState({
-    name: "",
     email: "",
   });
   const navigate = useNavigate();
+  const { showNotification } = useNotification();
 
   useEffect(() => {
     const registerToken = new URLSearchParams(window.location.search).get("token");
@@ -20,24 +25,29 @@ const Register = () => {
     if (!registerToken) {
       navigate("/login");
       return;
-    }
+    } 
+    
+    setFormData((prev) => ({
+      ...prev,
+      token: registerToken,
+      email: registerInfo.email,
+    }));
 
     const registerInfo = jwtDecode(registerToken);
     setRegisterInfo({
-      name: registerInfo.name,
       email: registerInfo.email,
     });
-    
+
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      console.log("formData", formData);
       await axiosInstance.post("/api/auth/register", formData);     
-      alert("Registration successful. Please log in.");
+      showNotification("Successfully registered. Please Log in", "success");
       navigate("/login");
     } catch (error) {
+      alert(`Login failed: ${error.response.data.message}`);
       alert(`Registration failed: ${error.response.data.message}`);
     }
   };
@@ -56,15 +66,6 @@ const Register = () => {
           <input
             type="email"
             value={registerInfo.email}
-            className="border border-gray-300 px-2 py-1 w-96 bg-gray-200 text-gray-400 cursor-not-allowed"
-            disabled
-          />
-        </div>
-        <div className="flex items-center justify-between">
-          <label className="pl-2 text-blue-500 font-bold">Name</label>
-          <input
-            type="text"
-            value={formData.name}
             className="border border-gray-300 px-2 py-1 w-96 bg-gray-200 text-gray-400 cursor-not-allowed"
             disabled
           />
